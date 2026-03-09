aa600a0bdb7da6c1e483d385a855116f69728b77
Igor Durigon <Igor.Durigon@ricsc.com.br>
2026-03-09 13:56:54 -0300

    style: customize for Grupo ND (atendimento and admin versions)

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2356,7 +2356,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://ndmais.com.br');
                   },
                   child: Text(
                     translate('Website'),
@@ -2374,7 +2374,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Grupo ND\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
