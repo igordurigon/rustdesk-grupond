af8b3d9c9fd82a0a5db0a557dc4f5af489fbe181
Igor Durigon <Igor.Durigon@ricsc.com.br>
2026-03-11 07:24:25 -0300

    fix: set default RS_PUB_KEY for Grupo ND

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -107,7 +107,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = option_env!("RS_PUB_KEY").unwrap_or("OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=");
+pub const RS_PUB_KEY: &str = option_env!("RS_PUB_KEY").unwrap_or("05Q9aFxELHvLZ4ZF4IxFyBq1rYjIvf2xV5j7qcKYrJQ=");
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
