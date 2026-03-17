9d3784965271a9ca6594bad4e652560f6495b806
Igor Durigon <Igor.Durigon@ricsc.com.br>
2026-03-17 11:13:33 -0300

    fix: disable 'lower version' warning by forcing is_installed_lower_version to false

--- src/ui_interface.rs
+++ src/ui_interface.rs
@@ -549,13 +549,7 @@ pub fn set_share_rdp(_enable: bool) {
 
 #[inline]
 pub fn is_installed_lower_version() -> bool {
-    #[cfg(not(windows))]
-    return false;
-    #[cfg(windows)]
-    {
-        let b = crate::platform::windows::get_reg("BuildDate");
-        return crate::BUILD_DATE.cmp(&b).is_gt();
-    }
+    false
 }
 
 #[inline]
