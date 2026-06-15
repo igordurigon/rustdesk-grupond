20d8107b6d07c21bc4b0de1397bc3149a2586e5c
Igor Durigon <Igor.Durigon@ricsc.com.br>
2026-06-15 08:16:30 -0300

    feat: adiciona builds macOS e Linux amd64/arm64 e corrige dominio do servidor

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -56,7 +56,7 @@ lazy_static::lazy_static! {
     static ref STATUS: RwLock<Status> = RwLock::new(Status::load());
     static ref TRUSTED_DEVICES: RwLock<(Vec<TrustedDevice>, bool)> = Default::default();
     static ref ONLINE: Mutex<HashMap<String, i64>> = Default::default();
-    pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("rustdesk.grupond.com.br".to_owned());
+    pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("rustdesk.grupond.tv.br".to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
     pub static ref APP_NAME: RwLock<String> = RwLock::new("Atendimento GrupoND".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
